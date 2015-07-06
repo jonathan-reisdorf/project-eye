37eaf56d85a55b89316d366a005239e3560770e4
Jonathan Reisdorf <fishlevel@crossnode.org>
2015-07-06 17:47:45 +0200

    initial

---
+++ server.js
@@ -0,0 +1,29 @@
+var gazejs = require("./gazejs")
+var eyeTracker = gazejs.createEyeTracker(gazejs.TOBII_GAZE_SDK);
+var listener = {
+    onStart:function(){
+        console.log("OnStart");
+    },
+    onStop:function(){
+        console.log("OnStop");
+    },
+    onError:function(error){
+        console.log(error);
+    },
+    onGazeData:function(gazeData){
+        console.log(gazeData);
+    }
+};
+
+eyeTracker.init();
+eyeTracker.setListener(listener);
+
+console.log("Library version: "+eyeTracker.getLibraryVersion());
+console.log("Model name: "+eyeTracker.getModelName());
+
+eyeTracker.start();
+
+/*Stop eye tracking after 20 seconds*/
+setTimeout(function(){
+    eyeTracker.release();
+},20000);
---
+++ startsrv.cmd
@@ -0,0 +1,2 @@
+call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86
+node server.js
