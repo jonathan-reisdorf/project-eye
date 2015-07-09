c740fdc6ca1ffdfc60efb17a477ad2fa36dec5c3
Jonathan Reisdorf <fishlevel@crossnode.org>
2015-07-09 11:39:12 +0200

    server: switched to express

--- server/_control.js
+++ server/_control.js
@@ -1,26 +1,22 @@
 module.exports = function(hardware) {
   'use strict';
 
-  var http    = require('http'),
+  var express = require('express'),
       faye    = require('faye'),
       deflate = require('permessage-deflate');
 
   var bayeux = new faye.NodeAdapter({ mount : '/control' });
   bayeux.addWebsocketExtension(deflate);
 
-  var client   = bayeux.getClient(),
-    httpServer = http.createServer(function(request, response) {
-      response.writeHead(200, { 'Content-Type': 'text/plain' });
-      response.end('ERR: no mount found!');
-    });
+  var client = bayeux.getClient(),
+    server = express();
 
-  bayeux.attach(httpServer);
-  httpServer.listen(7777);
-
+  bayeux.attach(server.listen(7777));
   console.log('Server is running on :7777');
+
   return {
     client : client,
-    server : httpServer,
+    server : server,
     hardware : hardware
   };
 };
