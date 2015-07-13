7c818829924ad7aadc3b30baa3649cdcaacdcfd5
Jonathan Reisdorf <fishlevel@crossnode.org>
2015-07-13 11:31:47 +0200

    client wip

--- client/assets/scripts/main.js
+++ client/assets/scripts/main.js
@@ -4,6 +4,7 @@ var $ = require('jquery');
 
 $(function() {
   var steps = {
+    testData : {},
     items : {
       calibrate : function() {
         $.get('//localhost:7777/tools/calibrate');
@@ -11,14 +12,48 @@ $(function() {
         window.setTimeout(function() {
           steps.next();
         }, 10000);
+      },
+      user : function($container) {
+        $container.find('input').off('keydown keyup change').on('keydown keyup change', function(e) {
+          $(this).siblings('button').attr('disabled', !$(this).val());
+        });
+
+        $container.find('form').off('submit').on('submit', function(e) {
+          e.preventDefault();
+          $(this).find('button').click();
+        });
+      },
+      prepare : function($container) {
+        $container.find('button').off('click').on('click', function() {
+          var frame = $('#test_frame').get(0);
+
+          if (frame.requestFullscreen) {
+            frame.requestFullscreen();
+          } else if (frame.msRequestFullscreen) {
+            frame.msRequestFullscreen();
+          } else if (frame.mozRequestFullScreen) {
+            frame.mozRequestFullScreen();
+          } else if (frame.webkitRequestFullscreen) {
+            frame.webkitRequestFullscreen();
+          }
+        });
+      },
+      start : function() {
+        $(document).off('webkitfullscreenchange mozfullscreenchange fullscreenchange').on('webkitfullscreenchange mozfullscreenchange fullscreenchange', function(e) {
+          var isFullScreen = document.fullscreen || document.mozFullScreen || document.webkitIsFullScreen;
+          if (!isFullScreen) {
+            steps.next('exit');
+          }
+        });
       }
     },
-    current : 'welcome',
+    current : 'user',
     queue : ['welcome', 'calibrate', 'user', 'prepare', 'start', 'exit'],
     init : function() {
       steps.next(steps.current);
 
       $('body').on('click', '[data-action="next"]', function() {
+        if ($(this).is(':disabled')) { return; }
         steps.next();
       });
     },
