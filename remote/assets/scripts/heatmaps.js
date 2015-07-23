4b47cce8a84120622489bcc70dcba0809e39da54
Jonathan Reisdorf <fishlevel@crossnode.org>
2015-07-23 16:27:51 +0200

    replay feature implemented

--- remote/assets/scripts/heatmaps.js
+++ remote/assets/scripts/heatmaps.js
@@ -46,5 +46,27 @@ module.exports = ['$rootScope', '$timeout', '$resource', 'CommonServers', functi
     self.active.last_page_url = heatmapItem.url;
   };
 
+  self.toggleReplay = function() {
+    if (!self.active) { return; }
+
+    self.active.eye = !self.active.eye;
+    var history = angular.copy(self.active.history);
+
+    var renderPoint = function() {
+      if (!self.active.eye || !history) { return; }
+      self.active.eye = history.shift();
+
+      if (history[0]) {
+        $timeout(renderPoint, history[0][2], true);
+      } else {
+        $timeout(function() {
+          self.active.eye = false;
+        }, 30, true);
+      }
+    };
+
+    $timeout(renderPoint, 0, true);
+  };
+
   return self;
 }];
